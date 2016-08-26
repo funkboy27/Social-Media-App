5e911d90ba9c218ec2a4626c455211fa15fc947e
funkboy27 <mattwells24@hotmail.com>
2016-08-26 14:04:22 -0700

    First Commit

---
+++ Social Media App/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Media App
+//
+//  Created by Matthew Wells on 2016-08-26.
+//  Copyright © 2016 Matthew Wells. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
